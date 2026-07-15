2c6a9ef26aab1ff3b5834978f6808c92375dc696
Sebastian 'Kuchen' Kucharczyk <kuchen@kekse.biz>
2026-07-15 11:52:07 +0200

    1784109125513527484

--- src/unit.js
+++ src/unit.js
@@ -872,21 +872,19 @@ Math.time.clock = (_data, _date) => {
 
 		if(char === ':')
 		{
-			if(state < 4)
+			if(_data[i + 1] === '*' && _data[i + 2] === '*')
 			{
-				if(result[state].length > __strLimit[state])
-				{
-					return null;
-				}
-
-				if(!checkInt())
-				{
-					return null;
-				}
+				continue;
 			}
-			else
+
+			if(result[state].length > __strLimit[state])
 			{
-				break parseLoop;
+				return null;
+			}
+
+			if(!checkInt())
+			{
+				return null;
 			}
 		}
 		else if(char === '+' || char === '-')
@@ -903,6 +901,23 @@ Math.time.clock = (_data, _date) => {
 			if(state === 0 && meridiem)
 			{
 				return null;
+			}
+
+			if(_data[i + 1] === '*')
+			{
+				if(!checkInt())
+				{
+					return null;
+				}
+
+				for(var j = state; j < 4; ++j)
+				{
+					result[j] = Math.time.
+						clock.getCurrent(
+							j, _date);
+				}
+
+				break;
 			}
 
 			if(result[state])
@@ -924,6 +939,11 @@ Math.time.clock = (_data, _date) => {
 		else
 		{
 			return null;
+		}
+
+		if(state > 3)
+		{
+			break;
 		}
 	}
 
